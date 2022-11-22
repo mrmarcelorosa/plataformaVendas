05a0c7fe9e13920bdfa69f85fb5f165a53d06c40
Marcelo Rosa <vyctor.rosa14@gmail.com>
2022-11-22 20:07:30 -0300

    Configure H2 database

--- src/main/java/com/example/vendas/domain/Categoria.java
+++ src/main/java/com/example/vendas/domain/Categoria.java
@@ -2,13 +2,23 @@ package com.example.vendas.domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Getter @Setter @AllArgsConstructor @NoArgsConstructor @EqualsAndHashCode
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionId = 1l;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column
     private String nome;
 }
