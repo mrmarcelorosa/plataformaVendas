b3f848ccda67a97a0db533689f04d56b8ffd1321
Marcelo Rosa <vyctor.rosa14@gmail.com>
2022-11-23 23:23:41 -0300

    Create instantiation operation

--- src/main/java/com/example/vendas/VendasApplication.java
+++ src/main/java/com/example/vendas/VendasApplication.java
@@ -1,13 +1,28 @@
 package com.example.vendas;
 
+import com.example.vendas.domain.Categoria;
+import com.example.vendas.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
